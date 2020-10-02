58d3676ccdb8a22ae576a5952d6405e56d7b3861
valealvarez2854 <61520291+valealvarez2854@users.noreply.github.com>
2020-10-02 00:04:29 -0500

    ya

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="calcular.php">
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Neuton:ital@1&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+</head  >
+<body>
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <img src="img/logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="logo1" loading="lazy">
+    <h1 class="navbar-brand">adidas</h1>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">pagina principal</a>
+        </li>
+        
+          <li class="nav-item">
+            <a class="nav-link" href="calcular.php">registro</a>
+          </li>
+          
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        
+        
+      </form>
+    </div>
+  </nav>
+</header>
+<mein>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/img8.jpg" class="d-block w-100 carusel" alt="img">
+      </div>
+      <div class="carousel-item">
+        <img src="img/img6.jpg" class="d-block w-100 carusel" alt="h">
+      </div>
+      <div class="carousel-item">
+        <img src="img/img3.jpg" class="d-block w-100 carusel" alt="img2">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+   
+  </div>
+  <div class="container">
+    <div class="row">
+    <div class="col-12 col-md-3">
+    <img src="img/img3.jpg" alt="gg">
+      <h6>deportivo mujer
+          $350.0000
+        </h6>
+        <img src="img/img7.jpg" alt="bb">
+      <h6>deportivo hombre
+      $360.0000
+        </h6>  
+    </div>
+    
+    <div class="col-12 col-md-3">
+    <img src="img/img11.jpg" alt="tt">
+    <h6>tenis hombre
+    466.000
+        </h6>  
+        <img src="img/img12.jpg" alt="123">
+      <h6>tenis mujer
+      $560.000
+        </h6>  
+  </div>
+    
+    <div class="col-12 col-md-3">
+    <img src="img/img6.jpg" alt="ch">
+    <h6>camisa mujer 
+        $148.900 
+        </h6>    
+        <img  src="img/img7.jpg" alt="pc">
+      <h6>camisa hombre
+         $149.900
+        </h6>  
+        </div>
+        <div class="col-12 col-md-3">
+        <img src="img/img8.jpg" alt="be">
+    <h6>  hombre
+    $299.900
+        </h6>    
+        <img src="img/img9.jpg" alt="vc">
+      <h6> mujer
+      $299.900
+        </h6>  
+       </div>
+  
+    </div>
+     
+    </div>
+</div>
+</mein>
+<footer>
+<hr>
+<ul>
+   <li>camila</li>
+  
+</ul>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
